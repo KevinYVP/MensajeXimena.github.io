e287f99db03e5db9ef328e762b3c89cb0907e140
Kevin Velasco <kevinyamid012@gmail.com>
2025-05-21 09:30:31 -0500

    Agregar el Proyecto

---
+++ script.js
@@ -0,0 +1,4 @@
+function fillWine() {
+    const wineLiquid = document.querySelector('.wine-liquid');
+    wineLiquid.style.height = '80%'; // Cambia este valor para ajustar la cantidad de vino que se llena
+  }
